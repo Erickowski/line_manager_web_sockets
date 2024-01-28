897fdcddc7f04946e0863cdee9201ddd8a71e387
Erick Daniel Perez Mata <erickdanielperezmata@outlook.es>
2024-01-27 23:06:12 -0600

    create ui for new ticket page

--- client/app/create-ticket/page.tsx
+++ client/app/create-ticket/page.tsx
@@ -1,3 +1,42 @@
+"use client";
+
+import { DownloadOutlined } from "@ant-design/icons";
+import { Button, Col, Flex, Row, Typography } from "antd";
+
+const { Title, Text } = Typography;
+
 export default function CreateTicket() {
-  return <p>CreateTicket</p>;
+  const handleNewTicket = () => {
+    console.log("Nuevo ticket");
+  };
+
+  return (
+    <>
+      <Row>
+        <Col span={20} offset={2}>
+          <Title level={3}>Presione el boton para crear un nuevo ticket</Title>
+          <Button
+            type="primary"
+            shape="round"
+            size="large"
+            icon={<DownloadOutlined />}
+            onClick={handleNewTicket}
+          >
+            Nuevo ticket
+          </Button>
+        </Col>
+      </Row>
+      <Row style={{ marginTop: 100 }}>
+        <Col span={24}>
+          <Flex justify="center" vertical align="center">
+            <Text>Su número</Text>
+            <br />
+            <Text type="success" style={{ fontSize: 55 }}>
+              55
+            </Text>
+          </Flex>
+        </Col>
+      </Row>
+    </>
+  );
 }
