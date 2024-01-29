9456c0bf6cc943660ec510e79e0e232ec4ad7ec7
Erick Daniel Perez Mata <erickdanielperezmata@outlook.es>
2024-01-28 18:08:52 -0600

    enable cors and create method to get last tickets assigned in server

--- server/models/server.ts
+++ server/models/server.ts
@@ -1,7 +1,8 @@
 const express = require("express");
 const http = require("http");
-const { Server } = require("socket.io");
 const path = require("path");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
 const { Sockets } = require("./socket");
 
@@ -10,28 +11,34 @@ export class ServerModel {
   port: string;
   server: any;
   io: any;
+  socket: any;
 
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
     this.server = http.createServer(this.app);
     this.io = new Server(this.server);
+    this.socket = new Sockets(this.io);
   }
 
   middlewares() {
     this.app.get("/", (_, res) => {
       res.sendFile(path.resolve(__dirname, "../public/index.html"));
     });
-  }
 
-  configureSockets() {
-    new Sockets(this.io);
+    // Configure cors
+    this.app.use(cors());
+
+    this.app.get("/last-tickets", (_, res) => {
+      res.json({
+        ok: true,
+        lastTicketsAssigned: this.socket.ticketList.getLastTickets,
+      });
+    });
   }
 
   execute() {
     this.middlewares();
-
-    this.configureSockets();
 
     this.server.listen(this.port, () => {
       console.log("listening on *:", this.port);
