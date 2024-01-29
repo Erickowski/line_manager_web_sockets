180a2678487127193e3d4f6e6578b58dc594ce60
Erick Daniel Perez Mata <erickdanielperezmata@outlook.es>
2024-01-28 18:45:36 -0600

    fix local storage not defined

--- client/utils/get-user-local-storage.ts
+++ client/utils/get-user-local-storage.ts
@@ -1,6 +1,13 @@
 import { LOCAL_STORAGE_KEYS } from "@/types";
 
 export function getUserLocalStorage() {
+  if (typeof window === "undefined") {
+    return {
+      username: null,
+      desktop: null,
+    };
+  }
+
   return {
     username: localStorage.getItem(LOCAL_STORAGE_KEYS.username),
     desktop: localStorage.getItem(LOCAL_STORAGE_KEYS.desktop),
